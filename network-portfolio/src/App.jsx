a7bc0a7d6f932631d2c32c76b44b22fd41d353e7
NP-OOP <ar112705@fhss.sjp.ac.lk>
2025-09-20 16:27:18 +0530

    Configure App.jsx with React Router and Navbar
    -Imported BrowserRouter, Routes, Route from react-router-dom.
    -Added routing for all page components (Home, About, Projects, NetworkTopologies, Contact, NotFound).
    -Integrated Navbar above all routes for navigation.

--- network-portfolio/src/App.jsx
+++ network-portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,7 +12,36 @@ function App() {
     </>
   )*/
 
-    return <div className="text-[50px]">Hello World</div>
+    //return <div className="text-[50px]">Hello World</div>
+//}
+
+//export default App
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import NetworkTopologies from "./pages/NetworkTopologies";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <Router>
+      {/* Navbar component with links and Dark Mode toggle*/}
+      <Navbar />
+
+      {/* Page routing using React Router */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home page with hero section + network canvas */}
+        <Route path="/about" element={<About />} /> {/* About page: background + skills */}
+        <Route path="/projects" element={<Projects />} /> {/* Projects page: cards with previews */}
+        <Route path="/network-topologies" element={<NetworkTopologies />} /> {/* Bonus interactive page */}
+        <Route path="/contact" element={<Contact />} /> {/* Contact page: form + validation */}
+        <Route path="*" element={<NotFound />} /> {/* 404 Page */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
